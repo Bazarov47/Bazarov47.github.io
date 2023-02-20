43a4785e44c2a52d9d44f24c23ac97f8c0de81fb
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2023-02-20 18:32:18 +0100

    Add files via upload
    - Customers Dashboard
    - Block a Customer
    - Mobile User-Friendly
    - Bug Fixes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "5a4586a551df5c5fbaca04ff3f4928c6",
-"/": "5a4586a551df5c5fbaca04ff3f4928c6",
-"main.dart.js": "0ba455ff9737d8f7f59b0cdf7544b30a",
+"index.html": "fc1fd222da06e62a9c0b1acdcaacf8b3",
+"/": "fc1fd222da06e62a9c0b1acdcaacf8b3",
+"main.dart.js": "a4c85b306d9f1e5c36c41511a4b6d470",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
