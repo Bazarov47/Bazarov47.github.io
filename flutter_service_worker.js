66e882a7201bb38fc25872bfb79aa767a5f0c131
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2023-04-26 12:32:24 +0200

    Added Last Login Field

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "e4f570d37dd2320be1396c42afe9b810",
-"/": "e4f570d37dd2320be1396c42afe9b810",
-"main.dart.js": "79270bdf9834ffbeabce23245eaceb7a",
+"index.html": "eb8ee9dad1cbd9fe372a8bd41821bb4c",
+"/": "eb8ee9dad1cbd9fe372a8bd41821bb4c",
+"main.dart.js": "7988e0e9c7774831e3701f7e89ac77da",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
