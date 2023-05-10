5c9c526f409404ae9877829d19454af19578297d
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2023-05-10 19:33:34 +0200

    Show Driver on Map when selected from List

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -34,9 +34,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "e7b176cf86bff642f37a44956022c709",
-"/": "e7b176cf86bff642f37a44956022c709",
-"main.dart.js": "bcc15b3fa72f26825d7424b05801dfae",
+"index.html": "f2eb6c7fe3c577cbb092d8f0fcb8c6e8",
+"/": "f2eb6c7fe3c577cbb092d8f0fcb8c6e8",
+"main.dart.js": "bf09d1290b25cde691cedff19df4efd5",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
