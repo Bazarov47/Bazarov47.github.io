4947958632fd5a548d89bcbb34ec12c06b593545
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2023-08-16 19:32:38 +0200

    Ride Comments - New Key

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "7f58492a9b713e144abb135438b72e85",
-"/": "7f58492a9b713e144abb135438b72e85",
-"main.dart.js": "1693199abeff9e223727ba5c4a1f57ec",
+"index.html": "cf1ffc9fb70417cd9e6d8b43123dfe74",
+"/": "cf1ffc9fb70417cd9e6d8b43123dfe74",
+"main.dart.js": "546562f40e219369e668ddd141ebecbe",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
