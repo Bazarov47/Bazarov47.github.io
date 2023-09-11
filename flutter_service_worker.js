31f76cec59b2d9ac06ad779380aa0ac62ac8e5f9
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2023-09-11 17:43:22 +0200

    Payment

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -39,9 +39,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "cf1ffc9fb70417cd9e6d8b43123dfe74",
-"/": "cf1ffc9fb70417cd9e6d8b43123dfe74",
-"main.dart.js": "546562f40e219369e668ddd141ebecbe",
+"index.html": "c35935be4a12cf25cf43745b2118eae4",
+"/": "c35935be4a12cf25cf43745b2118eae4",
+"main.dart.js": "8b5d27289d77716866be5afabde9439e",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
