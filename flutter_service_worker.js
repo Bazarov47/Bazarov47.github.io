a312d08dd1e9f235b295d2c4f6ba8fcd7603690b
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2023-10-03 15:06:11 +0200

    Add files via upload
    Bug Addresses

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -40,9 +40,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "791562df479c59af081cd4ba9f642882",
-"/": "791562df479c59af081cd4ba9f642882",
-"main.dart.js": "94856122f3de9aed6e5a7e567205f424",
+"index.html": "abf4e184a2104d4982a5e13798ad93e3",
+"/": "abf4e184a2104d4982a5e13798ad93e3",
+"main.dart.js": "9e43fef1098bac546c1c7776eeae42c0",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
