776bb68ac15bddb32c27f65aa1c62a7ba1a239b2
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2024-10-21 00:47:41 +0200

    Push Notifications
    Push Notifications

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,26 @@
+//importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js");
+//importScripts("https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.0.0/firebase-messaging-compat.js')
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCCLLuRp4QNAXWW6U7JWJ1fb7IDVamg70w",
+      authDomain: "vrapon-49003.firebaseapp.com",
+      projectId: "vrapon-49003",
+      storageBucket: "vrapon-49003.appspot.com",
+      messagingSenderId: "64011481291",
+      appId: "1:64011481291:web:eebac19e9b04a9ea396b4e",
+      measurementId: "G-PFS922WVYM"
+});
+
+const messaging = firebase.messaging();
+
+// Optional:
+messaging.onBackgroundMessage((message) => {
+  console.log("onBackgroundMessage", message);
+  const notificationTitle = message.notification.title;
+      const notificationOptions = {
+        body: message.notification.body,
+      };
+      self.registration.showNotification(notificationTitle, notificationOptions);
+});
