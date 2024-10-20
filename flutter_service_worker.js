0e8a6916805153759ab515a754e5ed7cb25698b6
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2024-10-21 01:45:38 +0200

    Push Bug 2
    Push Bug 2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,8 +43,8 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "d0324c80ddb3de2f0df6087030b18cfc",
-"/": "d0324c80ddb3de2f0df6087030b18cfc",
+"index.html": "02514060c627d5756c70a74d81a9312a",
+"/": "02514060c627d5756c70a74d81a9312a",
 "main.dart.js": "95b281ae13825b1a0838e8fbfe00d710",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
