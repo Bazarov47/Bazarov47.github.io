73b5d402a6414d5b4634c4bb9567b07536700ee4
Bazarov47 <33069340+Bazarov47@users.noreply.github.com>
2024-11-04 00:41:48 +0100

    Vehicle Management
    Vehicle Management

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -43,9 +43,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "75b90de5ee2183f1d7dfc19941d06347",
 "icons/Icon-maskable-192.png": "8bf7b8121658a48fed583773949faa4b",
 "icons/Icon-maskable-512.png": "75b90de5ee2183f1d7dfc19941d06347",
-"index.html": "46d570c05df121534e6996d82ca9d9f1",
-"/": "46d570c05df121534e6996d82ca9d9f1",
-"main.dart.js": "aaa97cbd2e48c37a1387e3db55b0f4bc",
+"index.html": "e71321377b9b4e367ed8346159266d2d",
+"/": "e71321377b9b4e367ed8346159266d2d",
+"main.dart.js": "a9b7f4f478b0a7896408315a3102ec67",
 "manifest.json": "66b6501725bfa6af3e096792b65e5a67",
 "version.json": "a17fac7a1ba7b5879f73019d57fab115"
 };
